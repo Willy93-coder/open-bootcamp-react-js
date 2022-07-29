0edf775d897562b6a15d79e8cab7a2c3b64dc4a9
Willy <guillecg13@gmail.com>
2022-07-29 18:56:34 +0200

    Debbuging y nuevas páginas vista

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import ContactList from "./components/container/contact_list";
+// import ContactList from "./components/container/contact_list";
+import TaskList from "./components/container/task_list";
 
 function App() {
     return (
         <div className="App">
-            <ContactList />
+            {/* <ContactList /> */}
+            <TaskList />
         </div>
     );
 }
